c4dd04b70241bea38cebd0195138ff426f005dfd
Marius <mariusjudickas@gmail.com>
2021-07-25 15:32:20 +0300

    2021-07-25

---
+++ 19_2021-07-26_PHP_arrays_GET_POST/homeworks_PHP/home_GET_POST_PHP/get_post.php
@@ -0,0 +1,4 @@
+<?php
+//Sukurti metoda su GET, kur butu vaizduojama dviju skaiciu daugyba.
+//Sukurti POST registracijos forma su pakartotiniu slaptazodziu ir butinais duomenimis.
+//Registracijos duomenis issiuskite i kita registration.php faila ir juos atvaizduokite.
---
+++ 19_2021-07-26_PHP_arrays_GET_POST/homeworks_PHP/home_game_PHP/game.php
@@ -0,0 +1,56 @@
+<?php
+
+// Parašykite funkciją, kuri mestų kauliuką iki kol iškris 5 arba 6. Visus savo metimus išvestų į ekrana.(while).
+
+while ($rollRes < 5) {
+ $rollRes = rand(1, 6);
+ echo('<pre>');
+ echo($rollRes);
+}
+ echo('<br>');    
+// Papildomas funkcionalumas žaidimui(papildomas sudėtingumas):
+// Metame 2 kauliukus (arba vieną kauliuką 2 kartus).
+// 	taisyklės (ziureti zemiau):
+
+//??????????????????????Nesugebejau pritaikyti While ciklo - reikia 
+
+while($resultReceived = false){
+	$rollRes1 = rand(1, 6);	
+	$rollRes2 = rand(1, 6);
+	$totalRes = $rollRes1 + $rollRes2;
+
+
+	var_dump($rollRes1);
+	var_dump($rollRes2);
+	var_dump($totalRes);
+
+
+	// 	Jeigu kauliukų suma yra mažiau už 5 reiškia - jūs iškarto pralaimejote.
+	if ($totalRes < 5){
+		var_dump('Rezultatas '.$totalRes .' reiškia - jūs iškarto pralaimejote.');
+	}
+		// 	Jeigu kauliukų suma daugiau už 5 ir:
+	// 	Išmesta buvo 6 ir 6, reiškia - laimėjote dviratį.
+	else if($rollRes1 == 6 && $rollRes2 == 6){
+		var_dump('Rezultatas '.$rollRes1 .'+' .$rollRes2 .'='.$totalRes .' reiškia - laimėjote dviratį.');
+	}
+
+	// 	Abiejų kauliukų išmestas skaičius yra vienodas, bet ne 6 ir 6 - reiškia jūs laimėjote bilietą į kiną.
+	else if ($totalRes > 5 && $rollRes1 == $rollRes2  && $rollRes1 !== 6 && $rollRes2 !== 6){
+		var_dump('Rezultatas '.$rollRes1 .'+' .$rollRes2 .'='.$totalRes .' Abiejų kauliukų išmestas skaičius yra vienodas, bet ne 6 ir 6 - reiškia jūs laimėjote bilietą į kiną.');
+
+	}
+		
+		// 	Kitais atvejais – kauliukas metamas dar kartą.
+	else {
+		return $resultReceived = false;
+		var_dump('Rezultatas '.$rollRes1 .'+' .$rollRes2 .'='.$totalRes .' kauliukas metamas dar kartą.');
+		
+		
+	}
+
+}
+	
+// Žaidimas turi vykti iki kol žaidėjas laimės / pralaimės(while)
+	
+// Visa žaidimo eiga turėtų būti išvesta į ekraną.
